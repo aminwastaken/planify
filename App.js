d48edd25aacf6d3a33d6846a39af7b4e91b54be9
amin <aminwastaken@gmail.com>
2022-03-18 10:48:14 +0100

    Implement navigation

--- App.js
+++ App.js
@@ -7,19 +7,30 @@ import {
   useColorScheme,
   View,
 } from 'react-native';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import {NavigationContainer} from '@react-navigation/native';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 import Text from './components/Text';
 import Home from './screens/Home';
+const Drawer = createDrawerNavigator();
+const Test = () => {
+  return <View></View>;
+};
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
-
   return (
-    <SafeAreaView>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <Home />
-    </SafeAreaView>
+    <NavigationContainer>
+      <Drawer.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerShown: false,
+        }}>
+        <Drawer.Screen name="Home" component={Home} />
+        <Drawer.Screen name="Test" component={Test} />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 };
 
